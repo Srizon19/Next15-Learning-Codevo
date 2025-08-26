c64b0c5229e23165f7b536c1f6dd02b9f58747de
Srizon19 <ra.srizon@gmail.com>
2025-08-26 19:16:52 +0600

    SETTING AND GETTING COOKIES FOR COMMENTS

--- route-handlers-backend/app/comments/route.js
+++ route-handlers-backend/app/comments/route.js
@@ -1,18 +1,29 @@
 import {comments} from '../commentData/data';
 import { NextRequest } from 'next/server';
-import { headers } from 'next/headers';
+import { headers, cookies } from 'next/headers';
 
 export async function GET(request){
     const {searchParams} = request.nextUrl;
     // const requestHeaders = new Headers(request.headers);
     const requestHeaders = await headers();
     console.log("here the auth token",requestHeaders.get('Authorization'));
+    
+    // accessing the cookie from the request header
+    const cookieData = request.cookies;
+    console.log("here is the cookie data",cookieData.get('sessionId'));
+    
+    // setting the cookie in the response header
+    const cookieStore = await cookies();
+    cookieStore.set('theme', 'dark');
+    console.log("here is the cookie data",cookieData.get('theme'));
+    
     const query = searchParams.get('query') || '';
     const filteredComments = query? comments.filter(c => c.text.toLocaleLowerCase().includes(query.toLocaleLowerCase())) : comments;
     return new Response(JSON.stringify(filteredComments), {
         status: 200,
         headers: {
             'Content-Type': 'application/JSON',
+            'set-Cookie': 'sessionId=abc123; Path=/; HttpOnly',
         },
     })  
 }
